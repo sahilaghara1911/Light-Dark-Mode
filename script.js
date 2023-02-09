dbb2f04099903eb480b53f9164d9895027588942
Sahil <nitinpatel9860@gmail.com>
2023-02-09 22:31:36 +0530

    Day4

---
+++ script.js
@@ -0,0 +1,49 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+//For light and dark mode image
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// Dark mode
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor ='rgb(255 255 255 / 50%)'
+    toggleIcon.children[0].textContent = 'Dark mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+    lightMode('dark')
+}
+
+// Light  mode
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor ='rgb(0 0 0 / 50%)'
+    console.log(toggleIcon.children);
+    toggleIcon.children[0].textContent = 'Light mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    imageMode('light')
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if(event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+        lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme)
